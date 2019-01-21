96e5bc82be9e87b20304f0dae114482b0078009e
Eldar Akhmetov <eldar.workx@gmail.com>
2019-01-21 20:53:07 +0700

    1.0.7

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('test');
+import program from 'commander';
+
+program
+  .description('Compares two configuration files and shows difference')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondCoifig>')
+  .parse(process.argv);
